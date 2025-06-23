23f5024b4eb8d5818fe9f63de50976e7a8fbdd96
Muneeb Mustafa <muneeb.mustafa080@gmail.com>
2025-06-23 18:50:04 +0500

    file changes

--- client/src/components/Contact/Hero.jsx
+++ client/src/components/Contact/Hero.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
-import { Breadcrumb } from "react-bootstrap";  
-import { Link } from "react-router-dom";  
-
+import React from 'react';
+import { Breadcrumb } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="sectionStyles">
+    <div
+      className="sectionStyles"
+      style={{
+        backgroundImage: "url('/images/contact.jpeg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        padding: "80px 20px",
+        color: "#fff",
+        textAlign: "center",
+      }}
+    >
       <h4
         style={{
           fontFamily: "Playfair-Display",
           fontSize: "60px",
+          marginBottom: "20px",
         }}
       >
         Contact Us
       </h4>
 
       <Breadcrumb>
-        <Breadcrumb.Item as={Link} to="/">Home</Breadcrumb.Item>
-        <Breadcrumb.Item active>Contact</Breadcrumb.Item>
+        <Breadcrumb.Item as={Link} to="/" style={{ color: "#fff" }}>
+          Home
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active style={{ color: "#ddd" }}>
+          Contact
+        </Breadcrumb.Item>
       </Breadcrumb>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
